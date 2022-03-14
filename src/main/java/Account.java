d229023ad4efafc31f8afaac11186189d4844de5
PaulaAndrz <pandrzejewska@o2.pl>
2022-03-14 19:48:03 +0100

    Add openAccount()

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,9 +1,12 @@
+import java.util.Scanner;
+
 public class Account {
 
     private int accountNo;
     private String accountName;
     private String accountSurmane;
     private long balance;
+    Scanner scanner = new Scanner(System.in);
 
     public Account(int accountNo, String accountName, String accountSurmane, long balance) {
         this.accountNo = accountNo;
@@ -25,6 +28,17 @@ public class Account {
 
     public void setAccountNo(int accountNo) {
         this.accountNo = accountNo;
+
+    }
+    public void openAccount(){
+        System.out.println("Podaj numer ID");
+        accountNo = scanner.nextInt();
+        System.out.println("Podaj imię");
+        accountName = scanner.next();
+        System.out.println(" Podaj nazwisko");
+        accountSurmane = scanner.next();
+        System.out.println(" Podaj stan konta");
+        balance = scanner.nextInt();
 
     }
 
